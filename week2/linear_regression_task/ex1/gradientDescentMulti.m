ae73660f105a6a3e939aafeed719a2a47f179f29
eugene <eugene.rublenko@gmail.com>
2016-08-19 20:23:16 +0300

    implement gradient descent for multiple variables

--- week2/linear_regression_task/ex1/gradientDescentMulti.m
+++ week2/linear_regression_task/ex1/gradientDescentMulti.m
@@ -17,8 +17,11 @@ for iter = 1:num_iters
     %       of the cost function (computeCostMulti) and gradient here.
     %
 
-
-
+    temp = zeros(size(theta));
+    for i=1:size(theta)
+        temp(i) = theta(i) - (alpha/m)*sum((X*theta-y).*X(:,i));
+    end
+    theta = temp;
 
 
 
