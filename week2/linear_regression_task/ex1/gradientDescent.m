d68fe60a1bcc26c00448865b506f97cbbcbe04f0
eugene <eugene.rublenko@gmail.com>
2016-08-19 15:32:56 +0300

    complete gradient descent algorithm

--- week2/linear_regression_task/ex1/gradientDescent.m
+++ week2/linear_regression_task/ex1/gradientDescent.m
@@ -17,10 +17,22 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
 
+    sum = 0;
+    for i = 1:m
+        sum += (theta' * (X(i,:))' - y(i))*X(i,1);
+    end;
+    temp1 = theta(1) - (alpha/m)*sum;
+    
+    sum = 0;
+    for i = 1:m
+        sum += (theta' * (X(i,:))' - y(i))*X(i,2);
+    end;
+    temp2 = theta(2) - (alpha/m)*sum;
 
+    theta(1) = temp1;
+    theta(2) = temp2;
 
-
-
+    
 
 
     % ============================================================
