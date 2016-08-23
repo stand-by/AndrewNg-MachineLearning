9ea4675330f219c9c1d8eb021b187f23865dc9ac
eugene <eugene.rublenko@gmail.com>
2016-08-23 12:11:58 +0300

    add appropriate files for multivariable linear regression realization in python

---
+++ week2/linear_regression_task/python_realization_of_multivariable/multivar_linear_regression.py
@@ -0,0 +1,8 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+if len(sys.argv) == 1:
+    print "You have to pass filename as argument"
+    exit() 
+(scriptname, filename) = sys.argv
