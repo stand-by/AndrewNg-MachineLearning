704cb3a3c63f32f829d23518a3372154b1785aad
eugene <eugene.rublenko@gmail.com>
2016-08-29 10:03:25 +0300

    implement multidimensional sigmoid function

--- week3/classification_task/python_realization_of_classification/classification.py
+++ week3/classification_task/python_realization_of_classification/classification.py
@@ -2,6 +2,9 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
+def sigmoid(z):
+    return 1/(1+np.exp(-z));
+
 if len(sys.argv) == 1:
     print "You have to pass filename as argument"
     exit() 
@@ -19,9 +22,11 @@ X = np.array([sample[:-1] for sample in data])
 y = np.array([sample[-1] for sample in data])
 m = len(y)
 
+#print X
+#print y
+
 plt.title("Logistical regression")
 for i in range(len(y)):
     if y[i] == 0: plt.plot(X[i,1], X[i,2], 'bo')
     if y[i] == 1: plt.plot(X[i,1], X[i,2], 'rx')
-plt.show()
-
+#plt.show()
