73fcd57fa63e43d218497b13f39b06da9489dcb0
eugene <eugene.rublenko@gmail.com>
2016-08-29 09:48:53 +0300

    implement plotting data in python

--- week3/classification_task/python_realization_of_classification/classification.py
+++ week3/classification_task/python_realization_of_classification/classification.py
@@ -19,7 +19,9 @@ X = np.array([sample[:-1] for sample in data])
 y = np.array([sample[-1] for sample in data])
 m = len(y)
 
-#print "X="
-#print X
-#print "y="
-#print y
+plt.title("Logistical regression")
+for i in range(len(y)):
+    if y[i] == 0: plt.plot(X[i,1], X[i,2], 'bo')
+    if y[i] == 1: plt.plot(X[i,1], X[i,2], 'rx')
+plt.show()
+
